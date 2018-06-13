cf366a5b5c71183163cae1ee0b842469c59a01ba
zhangyao <zhangyao@parim.net>
2018-06-13 20:33:56 +0800

    面积图标

--- src/app/components/react-page/react-page.component.ts
+++ src/app/components/react-page/react-page.component.ts
@@ -21,6 +21,9 @@ export class ReactPageComponent implements OnInit, AfterViewInit {
   _y: any;
   line: any;
   parseTime: any;
+  area: any;
+  content: any;
+  newdata: any = [];
   constructor() { }
 
   ngOnInit() {
@@ -46,69 +49,64 @@ export class ReactPageComponent implements OnInit, AfterViewInit {
     };
   }
   ngAfterViewInit() {
+    this.svg = d3.select('svg').attr('width', 650).attr('height', 320);
+    this.svg = d3.select('svg');
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = this.svg.attr('width') - margin.left - margin.right;
+    const height = this.svg.attr('height') - margin.top - margin.bottom;
+    this.box = this.svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')').attr('class', 'box');
+    this.content = d3.select('.box');
+    this.parseTime = d3.timeParse('%d-%b-%y');
 
+    this._x = d3.scaleTime()
+      .rangeRound([0, width]);
+
+    this._y = d3.scaleLinear()
+      .rangeRound([height, 0]);
+
+    this.area = d3.area()
+      .x((d) => {
+        return this._x(d.date);
+      })
+      .y1((d) => {
+         return this._y(d.close);
+       });
 
     d3.tsv('../../../assets/svg/data.tsv', (d) => {
       d.date = this.parseTime(d.date);
       d.close = +d.close;
-      return d;
-    }, (data) => {
-      // if (error) throw error;
-      this.svg = d3.select('svg').attr('width', 650).attr('height', 320);
-      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-      const width = this.svg.attr('width') - margin.left - margin.right;
-      const height = this.svg.attr('height') - margin.top - margin.bottom;
-      this.box = this.svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-      this.parseTime = d3.timeParse('%d-%b-%y');
-
-      this._x = d3.scaleTime()
-        .rangeRound([0, width]);
-
-      this._y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-
-      this.line = d3.line()
-        .x((d) => {
-          return this._x(d.date);
-        })
-        .y((d) => {
-          return this._y(d.close);
-        });
-
-      this._x.domain(d3.extent(data, (d) => {
-        return d.date;
-      }));
-      this._y.domain(d3.extent(data, (d) => {
-        return d.close;
-      }));
-
-      this.box.append('g')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(d3.axisBottom(this._x))
-        .select('.domain')
-        .remove();
-
-      this.box.append('g')
-        .call(d3.axisLeft(this._y))
-        .append('text')
-        .attr('fill', '#000')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '0.71em')
-        .attr('text-anchor', 'end')
-        .text('Price ($)');
-
-      this.box.append('path')
-        .datum(data)
-        .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round')
-        .attr('stroke-width', 1.5)
-        .attr('d', this.line);
+      this.newdata.push(d);
+      return this.newdata;
     });
+      // if (error) throw error;
+      console.log(this.newdata);
+    //   this._x.domain(d3.extent(data,  (d) => {
+    //     console.log(d);
+    //      return d.date;
+    //   });
+    //   this._y.domain([0, d3.max(data, (d) => {
+    //      return d.close;
+    //  })]);
+    //  this.area.y0(this._y(0));
 
+    //   this.content.append('path')
+    //     .datum(data)
+    //     .attr('fill', 'steelblue')
+    //     .attr('d', this.area);
+
+      // this.box.append('g')
+      //   .attr('transform', 'translate(0,' + height + ')')
+      //   .call(d3.axisBottom(this._x));
+
+      // this.box.append('g')
+      //   .call(d3.axisLeft(this._y))
+      //   .append('text')
+      //   .attr('fill', '#000')
+      //   .attr('transform', 'rotate(-90)')
+      //   .attr('y', 6)
+      //   .attr('dy', '0.71em')
+      //   .attr('text-anchor', 'end')
+      //   .text('Price ($)');
 
     // this.svg = d3.select('.chart').attr('width', 650).attr('height', 320);
     // const margin = { left: 20, right: 30, top: 30, bottom: 20 };
